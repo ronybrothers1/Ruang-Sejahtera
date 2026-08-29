36cec45b9c7452c4628135ab78c1eadc0d274580
ronybrothers1 <rony.bersahaja@gmail.com>
2026-08-29 23:25:21 +0700

    style: load V6 design system last

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import './v42.css';
 import './v43.css';
 import './v5.css';
 import './v51.css';
+import './v6.css';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 import { OrganizationJsonLd } from '@/components/OrganizationJsonLd';
